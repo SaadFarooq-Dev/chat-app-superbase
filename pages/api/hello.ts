4babbabe1504298e0cf073fa1cedefa689a51cad
Saad Farooq <saadfarooq.info@gmail.com>
2023-04-12 17:27:01 +0500

    Refactored code

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,13 +2,10 @@
 import supabase from '@/utils/supabase'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   const sendata = async () =>{
     const {data,error} = await supabase.from('messages_anon').insert({ content: "This is a message", username: (Math.random().toString(36).substring(2,7))}).select('*')
